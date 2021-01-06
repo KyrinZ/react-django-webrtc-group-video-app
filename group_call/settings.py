cd24430830b148d1560ad2af9e55abec2536a1af
KyrinZero <bayajitmohammed@gmail.com>
2021-01-06 08:17:02 +0800

    updated the changes

--- group_call/settings.py
+++ group_call/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "group_call.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    # "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"},
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "ddsurvn6a0keda",
